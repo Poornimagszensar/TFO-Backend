4f2d0711c9e07580801264505ee777d0a4d2d106
Poornimagszensar <poornima.gs@zensar.com>
2025-10-14 13:39:43 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # TFO-Backend
 Talent Flow Optimizer (TFO) - A GenAI-Powered Talent Intelligence Platform
+
+# Clone the Project using git 
+git clone https://github.com/Poornimagszensar/TFO-Backend.git
+
+# create a virtual environment 
+python -m venv venv
+
+# install dependencies
+pip install -r requirements.txt
+
+# run the streamlit application
+streamlit run multi_agent_frontend.py (application will start in the browser ex: http://localhost:8501/)
---
+++ multi_agent_frontend.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from multi_agent_workflow import workflow 
+from langchain_core.messages import HumanMessage
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+    
+st.title('Chatbot')
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input("Type here...")
+
+if user_input:
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    thread_id = 1
+    config = {'configurable': {'thread_id': thread_id}}
+    
+    with st.spinner("Processing..."):
+        response = workflow.invoke({
+            "messages": HumanMessage(content=user_input),
+        }, config=config)
+        ai_message = f"""Agent used: {response['agent']}
+Response: {response['messages'][-1].content}"""
+
+        st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+        with st.chat_message('assistant'):
+            st.text(ai_message)
